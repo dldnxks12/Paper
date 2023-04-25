dcc0911be0738b0188383b36554c1b768fd6451f
Jongsoo Lee <jjongs@postech.ac.kr>
2023-04-25 14:54:39 +0900

    Update and rename delayed_taxi.py to sarsa_ref.py

--- delayed control/delayed feedback/sarsa_ref.py
+++ delayed control/delayed feedback/sarsa_ref.py
@@ -1,19 +1,23 @@
-import gym
+
+import gymnasium as gym
 import sys
 import numpy as np
 import random
 from time import sleep
+
+
 
 def behavior_policy(random_policy, state, epsilon):
     if random.random() < epsilon:
         action = env.action_space.sample()
     else:
         action = np.argmax(random_policy[state, :])
+
     return action
+
 
 def Sarsa(env, random_policy,  Y, epsilon, n_epsiodes):
     Q = np.zeros([nS, nA])
-
     for e in range(n_epsiodes):
         state = env.reset()[0]  # Initial State
 
@@ -25,29 +29,27 @@ def Sarsa(env, random_policy,  Y, epsilon, n_epsiodes):
         truncated = False
 
         while True:
-
-            if random.random() < 0.1:
+            if random.random() < epsilon:
                 action = env.action_space.sample()
             else:
                 action = np.argmax(Q[state, :])
 
-            next_state, reward, done, truncated, _ = env.step(action) # action : 0
+            next_state, reward, done, done2, _ = env.step(action)
             total_reward += reward
-
             next_action = np.argmax(Q[next_state, :])
-            TD_error = (reward + gamma * Q[next_state][next_action]) - Q[state][action]
-            Q = Q + alpha * TD_error
+
+            Q[state][action] = ((1-alpha) * Q[state][action]) + (alpha*(reward + gamma * Q[next_state][next_action]))
 
             state = next_state
 
-            if done == True or truncated == True:
+            if done == True or done2 == True:
                 break
 
         Y.append(total_reward)
         print(f"Episode : {e} || total_reward : {total_reward}")
 
     Y = np.array(Y)
-    np.save('./dSarsa(λ)', Y)
+    np.save('./Sarsa_ref', Y)
     return Q
 
 Y = []
@@ -59,4 +61,4 @@ nS = env.observation_space.n
 nA = env.action_space.n
 random_policy = np.ones([nS, nA]) / nA
 
-Q = Sarsa(env, random_policy, Y , epsilon = 0.1, n_epsiodes = 50000)
+Q = Sarsa(env, random_policy, Y , epsilon = 0.0, n_epsiodes = 5000)
